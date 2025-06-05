6307bae9612c2c94ef32d5cb3146b3889ceec451
truongnhoxcon <nhattruong130824@gmail.com>
2025-06-05 15:49:00 +0700

    caesar bai 3

---
+++ lab03/caesar_cipher.py
@@ -0,0 +1,70 @@
+# caesar_cipher.py
+# Tác giả: MyApp -> Giao diện mã hóa / giải mã Caesar Cipher
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+import requests
+from ui.caesar import Ui_MainWindow  # Giao diện tạo từ Qt Designer
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Gắn sự kiện nút
+        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
+        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/caesar/encrypt"
+        payload = {
+            "plain_text": self.ui.txt_plain_text.toPlainText(),
+            "key": self.ui.txt_key.toPlainText()
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.txt_cipher_text.setText(data['encrypted_message'])
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Encrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error: API returned non-200 status code")
+
+        except requests.exceptions.RequestException as e:
+            print("Error while calling API:", e)
+
+    def call_api_decrypt(self):
+        url = "http://127.0.0.1:5000/api/caesar/decrypt"
+        payload = {
+            "cipher_text": self.ui.txt_cipher_text.toPlainText(),
+            "key": self.ui.txt_key.toPlainText()
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.txt_plain_text.setText(data['decrypted_message'])
+
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Decrypted Successfully")
+                msg.exec_()
+            else:
+                print("Error: API returned non-200 status code")
+
+        except requests.exceptions.RequestException as e:
+            print("Error while calling API:", e)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
