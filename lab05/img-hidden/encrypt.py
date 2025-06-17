31a45f6ac0a3427a48e2bade2f37afe4072668a9
truongnhoxcon <nhattruong130824@gmail.com>
2025-06-17 16:24:59 +0700

    add bai 5

--- lab05/img-hidden/encrypt.py
+++ lab05/img-hidden/encrypt.py
@@ -1,33 +1,40 @@
-# Phần 1: Khởi tạo, thêm giao dịch và đào block mới
-from blockchain import Blockchain
+import sys
+from PIL import Image
 
-# Testing the blockchain
-my_blockchain = Blockchain()
+def encode_image(image_path, message):
+    img = Image.open(image_path)
+    width, height = img.size
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    binary_message += '1111111111111110'  # đánh dấu kết thúc thông điệp
 
-# Adding transactions
-my_blockchain.add_transaction('Alice', 'Bob', 10)
-my_blockchain.add_transaction('Bob', 'Charlie', 5)
-my_blockchain.add_transaction('Charlie', 'Alice', 3)
+    data_index = 0
+    for row in range(height):
+        for col in range(width):
+            pixel = list(img.getpixel((col, row)))
 
-# Mining a new block
-previous_block = my_blockchain.get_previous_block()
-previous_proof = previous_block.proof
-new_proof = my_blockchain.proof_of_work(previous_proof)
-previous_hash = previous_block.hash
-my_blockchain.add_transaction('Genesis', 'Miner', 1)
-new_block = my_blockchain.create_block(new_proof, previous_hash)
+            for color_channel in range(3):
+                if data_index < len(binary_message):
+                    pixel[color_channel] = (pixel[color_channel] & ~1) | int(binary_message[data_index])
+                    data_index += 1
+            img.putpixel((col, row), tuple(pixel))
 
+            if data_index >= len(binary_message):
+                break
+        if data_index >= len(binary_message):
+            break
 
-# Phần 2: Hiển thị và xác thực blockchain
-# Displaying the blockchain
-for block in my_blockchain.chain:
-    print(f"Block #{block.index}")
-    print("Timestamp:", block.timestamp)
-    print("Transactions:", block.transactions)
-    print("Proof:", block.proof)
-    print("Previous Hash:", block.previous_hash)
-    print("Hash:", block.hash)
-    print("-------------------------")
+    encoded_image_path = "encoded_image.png"
+    img.save(encoded_image_path)
+    print("Steganography complete. Encoded image saved as", encoded_image_path)
 
-# Check if the blockchain is valid
-print("Is Blockchain Valid:", my_blockchain.is_chain_valid(my_blockchain.chain))
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python encrypt.py image_path message")
+        return
+
+    image_path = sys.argv[1]
+    message = sys.argv[2]
+    encode_image(image_path, message)
+
+if __name__ == "__main__":
+    main()
